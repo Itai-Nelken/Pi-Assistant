cfbb161f68cc5cae4f1982c4638b4582cd3999af
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 14:04:24 +0300

    Add files via upload
    improvement to install script

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo chmod +x system-tools.sh || echo "$(tput setaf 1)error occured! $(tput sgr 
 sudo chmod +x install2.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 ./install2.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 echo "$(tput setaf 2)Installation succesful! $(tput sgr 0)"
-echo exiting in 10 seconds...
-sleep 10
+echo "$(tput setaf 3)to run system-tools, run the desktop shortcut $(tput sgr 0)"
+echo exiting in 5 seconds...
+sleep 5
 exit
