92c22a0540164ada925506b81194770b6e583cac
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 13:13:55 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 cp ~/system-tools/system-tools.desktop ~/Desktop
 cd ~/system-tools
 sudo chmod +x system-tools.sh
+sudo chmod +x install2.sh
+./install2.sh
+echo "$(tput setaf 2)Installation succesful! $(tput sgr 0)"
+echo exiting in 10 seconds...
+sleep 10
+exit
