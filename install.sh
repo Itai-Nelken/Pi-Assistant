e71d5fc7952c1bd0a1ed2636ca623fa17ee6f740
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 14:05:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp ~/system-tools/system-tools.desktop ~/Desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 cd ~/system-tools || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x system-tools.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x install2.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
