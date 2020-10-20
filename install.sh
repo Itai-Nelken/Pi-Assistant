2026c46eef206140043dd75ec302159c619a45fe
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 14:39:06 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ~/system-tools/system-tools.desktop ~/Desktop || echo "$(tput setaf 1)faile to create desktop shortcut! $(tput sgr 0)"
-cp ~/system-tools/system-tools.desktop /usr/share/applications || echo "$(tput setaf 1)faile to create menu shortcut! $(tput sgr 0)"
+cp ~/system-tools/system-tools.desktop ~/Desktop || echo "$(tput setaf 1)failed to create desktop shortcut! $(tput sgr 0)"
+sudo cp ~/system-tools/system-tools.desktop /usr/share/applications || echo "$(tput setaf 1)failed to create menu shortcut! $(tput sgr 0)"
 cd ~/system-tools || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x system-tools.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x install2.sh || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
