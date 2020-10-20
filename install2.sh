a439e97801fa9fdb7ab22dea85c540023e975a45
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 14:05:30 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#create desktop shortcut
 cd ~ && cd Desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x system-tools.desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 
