c4c1d8038db007aa3f38447a2be6d019fd203324
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-20 14:44:26 +0300

    Add files via upload

--- install2.sh
+++ install2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#create desktop shortcut
-cp ~/system-tools/system-tools.desktop ~/Desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 cd ~ && cd Desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 sudo chmod +x system-tools.desktop || echo "$(tput setaf 1)error occured! $(tput sgr 0)"
 
