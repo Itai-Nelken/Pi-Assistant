95b21200f3c74ab07d51f85496d3dceebc700274
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-21 10:53:05 +0300

    fixed bug with naming so installation now works again

---
+++ system-tools-v1.1.1.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#run updater script
+./updater.sh
+
+#about & credits
+echo "$(tput setaf 3)system-tools v1.1.1 $(tput sgr 0)" 
+echo "$(tput setaf 3)by Itai Nelken $(tput sgr 0)"
+sleep 1 
+
+#main menu
+echo this script will help you install applications, update the system, and much more
+echo press [Ctrl-C] to exit
+PS3='Please enter the number above for operation you would like to perform (1 - 5): '
+options=("update" "change-password" "install-applications" "apt-tools" "other")
+select opt in "${options[@]}"
+do
+    case $opt in
+
+
+        "update")
+        
+./update.sh
+            break
+            ;;
+
+
+        "change-password")
+
+./change-password.sh
+
+clear
+
+            break
+            ;;
+
+
+
+ 
+        "install-applications")
+
+clear
+
+cd ~/Desktop/system-tools-beta/apps/
+./app-installer.sh
+
+clear
+
+            break
+            ;;
+
+
+
+        "apt-tools")
+
+./apt-tools.sh
+clear
+
+            break
+            ;;
+
+     "other")
+
+./other.sh
+clear
+
+            break
+            ;;
+
+
+
+
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+clear
+sleep 1
+echo operation complete. When ready, 
+read -p "press [Enter] to return to main menu or [Ctrl-C] to exit"
+./system-tools-v1.1-beta.sh
+
