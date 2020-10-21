b7f707d3f2cb023b097abc46381770324c13d6b7
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-21 13:53:00 +0300

    bug fixed in install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo cp ~/system-tools/system-tools.desktop /usr/share/applications || echo "$(t
 cd ~/system-tools || error "Failed to change to /home/pi/system-tools"
 sudo chmod +x system-tools-v1.1.1.sh || error "failed to mark system-tools-v1.1.1.sh as executable!"
 sudo chmod +x updater.sh || error "failed to mark updater.sh as executable!"
+sudo chmod +x other.sh || error "failed to mark other.sh as executable!"
 sudo chmod +x uninstall.sh || error "failed to mark uninstall.sh as executable!"
 sudo chmod +x install2.sh || error "failed to mark install2.sh as executable!"
 ./install2.sh || error "failed to run install4pi-apps.sh!"
