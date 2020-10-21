58de6b3083fd44f36f4d323cae8cbf02ee2faf5e
Itai-Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-21 14:13:00 +0300

    Add files via upload

--- updater.sh
+++ updater.sh
@@ -1,5 +1,6 @@
 #!/bin/bash  
 echo checking for updates...
+cd ~/system-tools
 git pull || error "error occured! can't look for updates are you connected to the internet?" 
-sleep 2
+sleep 1
 clear
