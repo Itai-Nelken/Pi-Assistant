624c297ff16f46e8f347ce941d00d049ce196e78
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-22 22:49:15 +0300

    Update zoominstaller-v0.9.1.sh

--- apps/zoom/zoominstaller-v0.9.1.sh
+++ apps/zoom/zoominstaller-v0.9.1.sh
@@ -5,11 +5,22 @@
 #wget https://example/file.??
 
 #don't think is needed
-cd ~/system-tools/apps/zoom
+#cd ~/system-tools/apps/zoom
 
-sudo dpkg -i box86.deb
+sudo dpkg -i box86-armhf.deb
+
+#download some tools needed
+sudo apt install xz-utils
+
+#download zoom (other linux OS>32 bit>Download)
+
+#extract zoom to /home/Pi/zoom and then delete the .tar.xz
 
 
-rm -f box86.deb
 
-exit 0
+#copy the desktop shortcut to the desktop and main menu
+cp ~/system-tools/apps/zoom/zoom.desktop ~/Desktop || error "failed to create desktop shortcut"
+sudo cp ~/system-tools/apps/zoom/zoom.desktop /usr/share/applications || error "failed to create menu shortcut!"
+cp ~/system-tools/apps/zoom/libxcb-xtest.so.0 ~/zoom || error "failed to copy libxcb-xtest.so.0 to ~/zoom"
+
+
