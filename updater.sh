ba6039f835169bb38fd1340a95c032bbfd7b8364
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-22 18:26:23 +0300

    trying to fix updater

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 echo checking for updates...
-git reset --hard
+#git reset --hard
 git pull || error "error occured! can't look for updates are you connected to the internet?" 
 sleep 2
 clear
