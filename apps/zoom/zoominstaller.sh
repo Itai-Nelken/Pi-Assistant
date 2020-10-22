27f80e8d8dc127dcd3a8c27c5bc6c4c85ad1a4b6
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-22 19:54:36 +0300

    Update zoominstaller.sh

--- apps/zoom/zoominstaller.sh
+++ apps/zoom/zoominstaller.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#commands to remember
+#1. find /where/to/look/up criteria action
+
+#2.how to compile box86:
+#git clone https://github.com/ptitSeb/box86
+#cd box86
+#mkdir build; cd build; cmake .. -DRPI4=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+#make -j24
+#sudo make install
+#sudo systemctl restart systemd-binfmt
+
+
+#if box86 is installed then ask if you want to reinstall, if box86 isn't installed, proceed with compile.
+if
+	
+	
+then
