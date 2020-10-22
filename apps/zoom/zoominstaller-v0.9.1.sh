076423f8fdcb6858333c2316b025058d6703f857
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-22 22:45:28 +0300

    Create zoominstaller-v0.9.1.sh

---
+++ apps/zoom/zoominstaller-v0.9.1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#big thanks to ptitSeb for creating box86: https://github.com/ptitSeb/box86
+
+#download file
+#wget https://example/file.??
+
+#don't think is needed
+cd ~/system-tools/apps/zoom
+
+sudo dpkg -i box86.deb
+
+
+rm -f box86.deb
+
+exit 0
