43d7c5c43ba03f2f5039d16960062a5f123fac96
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-23 00:51:51 +0300

    updated zoom installer

---
+++ apps/zoom/zoominstaller-0.9.2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#big thanks to ptitSeb for creating box86: https://github.com/ptitSeb/box86
+
+#if box86 is installed then ask if you want to reinstall, if box86 isn't installed, proceed with install.
+if
+	if
+		#is box86 installed? (search for ~/box86 folder)
+		
+		
+	then
+		#ask if you want to reinstall, if no, exit both loops
+	
+	
+	
+then
+	sudo dpkg -i box86-armhf.deb
--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+#copy desktop shortcut to desktop and main menu
 cp ~/system-tools/system-tools.desktop ~/Desktop || error "failed to create desktop shortcut!"
 sudo cp ~/system-tools/system-tools.desktop /usr/share/applications || error "failed to create menu shortcut!"
+
+#mark as executable some neded scripts
 cd ~/system-tools || error "Failed to change to /home/pi/system-tools"
 sudo chmod +x system-tools-v1.1.1.sh || error "failed to mark system-tools-v1.1.1.sh as executable!"
 sudo chmod +x updater.sh || error "failed to mark updater.sh as executable!"
 sudo chmod +x other.sh || error "failed to mark other.sh as executable!"
 sudo chmod +x uninstall.sh || error "failed to mark uninstall.sh as executable!"
+
+#mark as executable and run 2nd install script
 sudo chmod +x install2.sh || error "failed to mark install2.sh as executable!"
 ./install2.sh || error "failed to run install4pi-apps.sh!"
+
+#print in green 'installation succesful' and in orange how to run system tools
 echo "$(tput setaf 2)Installation succesful! $(tput sgr 0)"
 echo "$(tput setaf 3)to run system-tools, run the desktop shortcut or main menu>other>system-tools  $(tput sgr 0)"
+
+#print 'exiting in 5 seconds', wait 5 seconds and exit
 echo exiting in 5 seconds...
 sleep 5
 exit
