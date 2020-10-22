bee7d349ea383f2bb8b62239a6aba26b9f625b3b
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-22 21:54:21 +0300

    Update startzoom.sh

--- apps/zoom/startzoom.sh
+++ apps/zoom/startzoom.sh
@@ -3,6 +3,10 @@
 #go to zoom directory (~/zoom)
 cd ~/zoom
 
+echo "$(tput setaf 3)close this window to exit zoom$(tput sgr 0)"
+sleep 3
 echo "$(tput setaf 2)starting zoom...$(tput sgr 0)"
 #start zoom with box86
-box86 zoom
+box86 zoom || error "can't start zoom! are box86 and/or zoom installed?"
+echo "$(tput setaf 2)exiting in 5 seconds:($(tput sgr 0)"
+sleep 5
