81e37b886423efc214b8dc9e8e2d7c7ae98347c5
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-30 19:35:05 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -19,5 +19,5 @@ cd ..
 cd ~/Desktop || error "Failed to change to /home/pi/desktop/!"
 sudo chmod +x system-tools.desktop || error "failed to mark desktop shortcut as executable!"
 
-
+./install.sh
 
