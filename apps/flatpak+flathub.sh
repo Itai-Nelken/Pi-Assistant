ca3384dd10233e5554abe059c9cb8d89b9967c6c
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-31 19:12:36 +0200

    Update flatpak+flathub.sh

--- apps/flatpak+flathub.sh
+++ apps/flatpak+flathub.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
 echo this script will help you install or remove flatpak
 echo after installation you have to reboot, are you sure you want to continue?
 PS3='Please enter the number for install or remove (1 - 2): '
@@ -14,7 +19,7 @@ do
 sudo apt update || error "error occured! are you connected to the internet?"
 echo "$(tput setaf 3)installing snapd... $(tput sgr 0)"
 sudo apt install flatpak || error "error occured! are you connected to the internet?"
-echo do you want to add the flathub repository (recommended)?
+echo "do you want to add the flathub repository (recommended)?"
 PS3='Please enter the number for add or skip (1 - 2): '
 options=("skip" "add")
 select opt in "${options[@]}"
@@ -45,3 +50,6 @@ done
             break
             ;;
    
+   *) echo "invalid option $REPLY";;
+    esac
+done
