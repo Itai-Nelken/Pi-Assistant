4845734d6f7d8f7407d324bc693d866ba6d06cf2
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-31 21:02:19 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -6,23 +6,24 @@ function error {
 }
 
 cd apps
-sudo chmod +x app-installer.sh
-sudo chmod +x app-installer2.sh
-sudo chmod +x audacity.sh
-sudo chmod +x blender.sh
-sudo chmod +x chromium.sh
-sudo chmod +x firefox-esr.sh
-sudo chmod +x flatpak+flathub.sh
-sudo chmod +x gparted.sh
-sudo chmod +x gsystem-monitor.sh
-sudo chmod +x inkscape.sh
-sudo chmod +x kdenlive.sh
-sudo chmod +x snapd-snap-store.sh
-sudo chmod +x SSR.sh
+sudo chmod +x app-installer.sh || error "failed to mark app-installer.sh as executable!"
+sudo chmod +x app-installer2.sh || error "failed to mark app-installer2.sh as executable!"
+sudo chmod +x audacity.sh || error "failed to mark audacity.sh as executable!"
+sudo chmod +x blender.sh || error "failed to mark blender.sh as executable!"
+sudo chmod +x chromium.sh || error "failed to mark chromium.sh as executable!"
+sudo chmod +x firefox-esr.sh || error "failed to mark firefox-esr.sh as executable!"
+sudo chmod +x flatpak+flathub.sh || error "failed to mark flatpak+flathub.sh as executable!"
+sudo chmod +x gparted.sh || error "failed to mark gparted.sh as executable!"
+sudo chmod +x gsystem-monitor.sh || error "failed to mark gsystem-monitor.sh as executable!"
+sudo chmod +x inkscape.sh || error "failed to mark inkscape.sh as executable!"
+sudo chmod +x kdenlive.sh || error "failed to mark kdenlive.sh as executable!"
+sudo chmod +x snapd-snap-store.sh || error "failed to mark snapd-snap-store.sh as executable!"
+sudo chmod +x SSR.sh || error "failed to mark SSR.sh as executable!"
 cd ..
 sudo chmod +x other.sh || error "failed to mark other.sh as executable!"
 sudo chmod +x uninstall.sh || error "failed to mark uninstall.sh as executable!"
 sudo chmod +x updater.sh || error "failed to mark updater.sh as executable!"
+sudo chmod +x apt-tools.sh || error "failed to mark apt-tools.sh as executable!"
 
 cd ~/Desktop || error "Failed to change to /home/pi/desktop/!"
 sudo chmod +x system-tools.desktop || error "failed to mark desktop shortcut as executable!"
