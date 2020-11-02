f87a4d3e71f9f4a1378a327f7e8c0bf1c8dd29f5
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-02 18:33:10 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-rm -rf ~/system-tools || error "failed to remove system-tools folder!"
+rm -rf ~/Pi-Assistant || error "failed to remove Pi-Assistant folder!"
 sudo rm -f ~/Desktop/system-tools.desktop || error "failed to remove desktop shortcut!"
 sudo rm -f /usr/share/applications/system-tools.desktop || error "failed to remove menu button!"
 echo "$(tput setaf 3)system-tools removed:($(tput sgr 0)"
