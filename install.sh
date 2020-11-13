73918f1f5ca3eac4a1bc5c3aaebe8fd2daa38fed
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-13 15:24:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,13 @@ sudo chmod +x other.sh || error "failed to mark other.sh as executable!"
 sudo chmod +x uninstall.sh || error "failed to mark uninstall.sh as executable!"
 sudo chmod +x updater.sh || error "failed to mark updater.sh as executable!"
 
-
+#add startup from terminal using 'piassist'
+echo "#!/bin/bash
+clear && cd ~/Pi-Assistant/ && ./system-tools-v1.1.1.sh" > ~/Downloads/piassist
+sudo mv ~/Downloads/piassist /usr/local/bin/
+cd /usr/local/bin
+sudo chmod +x piassist
+cd ~/Pi-Assistant
 
 #mark as executable and run 2nd install script
 sudo chmod +x install2.sh || error "failed to mark install2.sh as executable!"
