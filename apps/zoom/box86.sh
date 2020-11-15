c426f338937bd8966d5b7e2ab690854169b65f62
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-15 13:34:53 +0200

    improvements to zoom install script

--- apps/zoom/box86.sh
+++ apps/zoom/box86.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+#install dependencies for box86
+sudo apt install cmake || error "failed to install cmake! are you connected to the internet?"
+
 cd ~
 git clone https://github.com/ptitSeb/box86 || error "failed to download repo!"
 cd box86 || error "failed to enter directory!"
