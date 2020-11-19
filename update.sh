89cda16b0cf72a3b6e64e8ca6954fb7255a205a4
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-19 23:35:20 +0200

    improvements to update script

--- update.sh
+++ update.sh
@@ -1,14 +1,17 @@
-#!/bin/bash   
+#!/bin/bash  
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
 
 echo you might see some errors if snap-store is not installed
 sleep 2
 sudo apt update || error "error occured! are you connected to the internet?"
 sleep 2
 clear
-echo when asked if you want to continue press 'y' followed by 'Enter'
-read -p "Press [Enter] to continue..."
 
-sudo apt upgrade || error "error occured are you connected to the internet?"
+sudo apt full-upgrade -y || error "error occured are you connected to the internet?"
 sleep 2
 sudo snap refresh snap-store || error "snapd or snap-store not installed"
 sleep 1
