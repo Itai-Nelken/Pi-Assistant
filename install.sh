13a1764cf20ba9a83947e1614eb25e2e2775444e
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-12-15 17:23:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ function error {
   exit 1
 }
 
+cd $HOME
+git clone https://github.com/Itai-Nelken/Pi-Assistant.git; cd ~/Pi-Assistant
+
 #copy desktop shortcut to desktop and main menu
 cp ~/Pi-Assistant/system-tools.desktop ~/Desktop || error "failed to create desktop shortcut!"
 sudo cp ~/Pi-Assistant/system-tools.desktop /usr/share/applications || error "failed to create menu shortcut!"
