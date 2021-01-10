b359b66fccea4be09830cec22950c21d5500351e
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-10 14:48:10 +0200

    Update ssr.sh

--- apps/ssr.sh
+++ apps/ssr.sh
@@ -1,16 +1,9 @@
-#!/bin/bash 
+#!/bin/bash  
 
-function error {
-  echo -e "\e[91m$1\e[39m"
-}
-
-#variables
-DIRECTORY="$HOME/Pi-Assistant"
-APPS="$HOME/Pi-Assistant/apps"
-
+clear
 echo "this script will help you install or remove simplescreenrecorder."
-PS3='Please enter the number for install or remove (1 - 3): '
-options=("install" "remove" "back to app installer")
+PS3='Please enter the number for install or remove (1 - 2): '
+options=("install" "remove")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -19,29 +12,27 @@ do
 
         "install")
 
-sudo apt update || error "Failed to update repos (not critical)."
-sudo apt install -y simplescreenrecorder || error "Failed to install simplescreenrecorder!"
-
-            break
-            ;;
-   
-   "remove")  
-   
-sudo apt purge -y simplescreenrecorder || error "failed to remove simplescreenrecorder!"
-   
-            break
-            ;;
-
-"back to app installer")
-   
-$APPS/appinstaller.sh
+echo "$(tput setaf 3)installing simplescreenrecorder...$(tput sgr 0)"
+sudo apt update || error "error occured! are you connected to the internet?"
+sudo apt install -y simplescreenrecorder || error "error occured! are you connected to the internet?"
+echo "$(tput setaf 2)simplescreenrecorder installed$(tput sgr 0)"
 
             break
             ;;
 
-       *) echo "invalid option $REPLY";;
+ "remove")
+
+echo "$(tput setaf 3)removing simplescreenrecorder...$(tput sgr 0)"
+sudo apt remove -y inkscape || error "error occured! can't remove simplescreenrecorder"
+clear
+echo "$(tput setaf 2)simplescreenrecorder removed$(tput sgr 0)"
+
+            break
+            ;;
+
+      *) echo "invalid option $REPLY";;
     esac
-done     
+done
 
-sleep 2
+sleep 1
 clear
