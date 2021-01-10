935f3f76496770f2b5e7de3491b0af1ba9bd9efd
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-10 15:18:04 +0200

    rewrite systools.sh using the template

--- systools.sh
+++ systools.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 function error {
   echo -e "\e[91m$1\e[39m"
@@ -8,38 +8,44 @@ function error {
 DIRECTORY="$HOME/Pi-Assistant"
 APPS="$HOME/Pi-Assistant/apps"
 
-echo "this script will help you install or remove audacity."
-PS3='Please enter the number for install or remove (1 - 3): '
+
+echo "this script will help you execute system maintenance tasks"
+PS3='Please enter the number above for operation you would like to perform (1 - 5): '
 options=("autoremove" "autoclean" "fix broken install" "clear swap" "back to main menu")
 select opt in "${options[@]}"
 do
     case $opt in
 
 
-
-    "autoremove")
-
+        "autoremove")
+        
 sudo apt autoremove -y || error "error occured!"
 
             break
             ;;
-   
-   "autoclean")  
-   
+
+
+        "autoclean")
+
 sudo apt autoclean || error "error occured!"
 sudo apt clean || error "error occured!"
-   
+
             break
             ;;
 
-  "fix broken install")
+
+
+ 
+        "fix broken install")
 
 sudo apt install -f || error "error occured!"
 
             break
             ;;
 
- "clear swap")
+
+
+        "clear swap")
 
 echo checking space used.
 free -m || error "error occured!"
@@ -56,16 +62,18 @@ echo "$(tput setaf 2)operation complete.$(tput sgr 0)"
             break
             ;;
 
-"back to main menu")
-   
+
+
+     "back to main menu")
+
 $DIRECTORY/main.sh
 
             break
             ;;
 
-       *) echo "invalid option $REPLY";;
-    esac
-done     
 
-sleep 2
-clear
+
+
+        *) echo "invalid option $REPLY";;
+    esac
+done
