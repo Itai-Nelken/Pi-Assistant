d6efa14fc6b71bb5fea5a7ac5bc780ca5b13800d
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-12 13:38:24 +0200

    test commit

--- install.sh
+++ install.sh
@@ -99,7 +99,6 @@ sudo chmod +x apps/etcher.sh
 sudo chmod +x apps/zoom.sh
 sudo chmod +x apps/box86.sh
 
-
 #print in green 'installation succesful' and in orange how to run system tools
 echo "$(tput setaf 2)Installation succesful! $(tput sgr 0)"
 echo -e "$(tput setaf 3)to run Pi-Assistant, run the desktop shortcut or main menu>other>Pi-Assistant
