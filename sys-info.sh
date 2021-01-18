52925363ec0ba2f062c1809a4182a3c44d1fe33d
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-18 23:47:43 +0200

    Update sys-info.sh

--- sys-info.sh
+++ sys-info.sh
@@ -6,7 +6,7 @@ USER="`env | grep USER`"
 KERNEL="`uname -rm`"
 
 #display everything
-echo "username: $USER"
+echo "${USER:5}"
 if [[ $DE == *"xfce"* ]]; then
 	echo "Desktop Environment is: XFCE"
 elif [[ $DE == *"lxde"* ]]; then
@@ -18,4 +18,4 @@ else
 fi
 echo "kernel: $KERNEL"
 
-read -n 1 -s -r -p "Press any key to exit"
+read -n 1 -s -r -p "Press any key to go back to main menu"
