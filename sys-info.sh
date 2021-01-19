0350990fe5969380f0732fe2176df0090e2d0924
Itai-Nelken <itainelken@gmail.com>
2021-01-19 10:34:10 +0200

    update system information script

--- sys-info.sh
+++ sys-info.sh
@@ -16,6 +16,7 @@ MODEL="`cat /proc/cpuinfo | grep Model`"
 CSPEED="`lscpu | grep "CPU max" | cut -c22-25`"
 OS="`cat /etc/*-release | grep PRETTY | cut -c14-43`"
 SOC="`cat /proc/cpuinfo | grep Hardware | cut -c12-18`"
+CORES="`nproc`"
 SOCREV="`cat /proc/cpuinfo | grep Revision | cut -c12-18`"
 IP="`hostname -I | awk '{print $1}'`"
 
@@ -26,7 +27,23 @@ else
         TWISTER=1
 fi
 
+#determine GPU model
+if [[ "$MODEL" == *"Raspberry Pi 4"* ]]; then
+	GPU="Broadcom® VideoCore® VI"
+elif [[ "$MODEL" == *"Raspberry Pi 3"* ]]; then
+	GPU="Broadcom® VideoCore® IV"
+elif [[ "$MODEL" == *"Raspberry Pi 2"* ]]; then
+	GPU="Broadcom® VideoCore® IV"
+else
+	GPU="0"
+fi
 
+#get gpu 3d block (v3d) clock speed
+if [ -f "/opt/vc/bin/vcgencmd" ]; then
+    GPUSPEED="`/opt/vc/bin/vcgencmd measure_clock v3d | cut -c15-23 | cut -c1-3`"
+else
+	GPUSPEED=0
+fi
 
 #determine if host system is 64 bit arm64 or 32 bit armhf
 if [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 64)" ];then
@@ -69,19 +86,24 @@ echo "kernel architecture is: $KARCH"
 echo -e " "
 
 printf "$(tput bold)\\e[3;4;37mHardware information:\\n\\e[0m$(tput sgr 0)"
-#display CPU name and speed
+#display CPU info
 echo "System on Chip (SOC) name is: $SOC, Revision $SOCREV"
+echo "Number of CPU cores is: $CORES"
 echo "Processor (CPU) core/s name is: ${CPU:21}"
-echo "Processor clock speed is: $CSPEED mhz"
+echo "Processor maximum clock speed is: $CSPEED mhz"
+#display GPU info
+if [[ "$GPU" != 0 ]]; then
+echo "GPU model is: $GPU"
+fi
+if [[ "$GPUSPEED" != 0 ]]; then
+echo "current GPU clock speed is: $GPUSPEED mhz"
+fi
 #display memory
 echo "memory (RAM) size is $MEM mb"
 
 echo " "
 
 printf "$(tput bold)\\e[3;4;37mSoftware information:\\n\\e[0m$(tput sgr 0)"
-printf "\\e[3;4;37mKernel:\\n\\e[0m"
-#display kernel
-echo "kernel: $KERNEL, release: $KRELEASE"
 printf "\\e[3;4;37mOperating system:\\n\\e[0m"
 #display OS
 echo "OS name is: $OS"
@@ -89,6 +111,9 @@ if [[ "$TWISTER" == 1 ]]; then
 	TWISTVER="`twistver | cut -c20-24`"
 	echo "OS mod is: TwisterOS version $TWISTVER"
 fi
+printf "\\e[3;4;37mKernel:\\n\\e[0m"
+#display kernel
+echo "kernel: $KERNEL, release: $KRELEASE"
 
 echo "                        "
 #display DE
