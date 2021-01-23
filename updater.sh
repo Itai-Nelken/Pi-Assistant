c3e35b46f9e160023e597c6194d083a503f2d72b
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-23 21:13:36 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -90,6 +90,7 @@ sudo chmod +x ~/Desktop/piassist.desktop
 sudo rm /usr/local/bin/piassist
 #update startup from terminal using 'piassist'
 echo '#!/bin/bash
+cd ~/Pi-Assistant
 #flags
 if  [[ $1 = "--version" ]]; then
     clear
@@ -103,26 +104,14 @@ elif [[ $1 = "--secret" ]]; then
     read -p "press [ENTER] to exit"
     exit
 elif [[ $1 = "--no-internet" ]]; then    
-    export NOINTERNETCHECK=1
-    export UPDATE=0
+    ./main.sh --no-internet
 elif [[ $1 = "--help" ]]; then
-    echo -e "
-    $(tput setaf 6)$(tput bold)usage:$(tput sgr 0)
-    piassist [flag]
-   $(tput setaf 6)$(tput bold)available flags:$(tput sgr 0)
-   --no-internet - dont check for internet connection on startup .
-   --version - show version and exit.
-   --secret - secret easter egg.
-   --help - show this help info and exit.
-   $(tput setaf 6)$(tput bold)example:$(tput sgr 0)
-   piassist --secret
-   "
-   #read -p "press [ENTER] to exit..."
-   exit
+    ./main.sh --help
 elif [[ $1 = "--no-update" ]]; then
-    export UPDATE=0
-fi
-clear && $HOME/Pi-Assistant/main.sh' > ~/Downloads/piassist
+    ./main.sh --no-update
+else
+    clear && $HOME/Pi-Assistant/main.sh
+fi' > ~/Downloads/piassist
 sudo mv ~/Downloads/piassist /usr/local/bin/
 sudo chmod +x /usr/local/bin/piassist
 
